99987aa88a98415d073e2dc8afe17995cf176f4d
Taleep <85577933+Taleep@users.noreply.github.com>
2021-08-23 02:17:08 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+	$('.slider__box').slick({
+		prevArrow: '<img src="images/Arrow-left.svg" alt="" class="slider__arrow slider__arrow-left">',
+		nextArrow: '<img src="images/Arrow-right.svg" alt="" class="slider__arrow slider__arrow-right">'
+	});
+});
